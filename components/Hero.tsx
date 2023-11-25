9f3d700c1d830a28a3a7bde863d464e46fb1d0ff
Izxbelaa <izabelaivanova7777@icloud.com>
2023-11-25 12:21:24 +0200

    Add existing file

---
+++ components/Hero.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import Button from './Button'
+
+const Hero = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <div className="hero-map" />
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        
+        <h1 className="bold-52 lg:bold-88 text-primary">Bus QR: Easy Travel, Smart Commute</h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        Learn about our mission to revolutionize public transportation by integrating QR codes and real-time data. Discover how we're making commuting more efficient and enjoyable
+        </p>
+
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+           
+          </div>
+
+          
+        </div>
+
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button 
+            type="button" 
+            title="Learn more" 
+            variant="btn_green" 
+          />
+          <Button 
+            type="button" 
+            title="How we work?" 
+            icon="/play.svg"
+            variant="btn_white_text" 
+          />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-primary px-7 py-8">
+
+           <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-neontext">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Molos </p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-neontext">Arrival Time</p>
+              <p className="bold-20 text-white">8 min</p>
+            </div>
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Bus</p>
+              <p className="bold-20 text-white">11</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
